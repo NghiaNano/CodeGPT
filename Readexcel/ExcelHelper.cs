d36bd0406f4dc5458f31b8452f1b2f711c82a773
NghiaNano <163148999+NghiaNano@users.noreply.github.com>
2024-03-25 08:19:50 +0700

    ReadExcel

---
+++ Readexcel/ExcelHelper.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadExcel
+{
+	public class ExcelHelper
+	{
+		private Application excelApp;
+		public int StartColumn { get;set; }
+		public int StartRow { get;set; }
+		public ExcelHelper() 
+		{
+			excelApp = new Application();
+		}
+		public void CopyRange (string sourcepath, string despath, string sourceSheetName, string desSheetName, string sourceRangeAdress, string desRangeAdress)
+		{
+			//Source
+			Workbook sourceWorkbook = excelApp.Workbooks.Open(sourcepath);
+			Worksheet sourceworksheet = (Worksheet)sourceWorkbook.Sheets[sourceSheetName];
+			Excel.Range sourceRange = sourceworksheet.Range[sourceRangeAdress];
+
+			//Dest
+			Workbook desWorkbook = excelApp.Workbooks.Open(despath);
+			Worksheet desworksheet = (Worksheet)desWorkbook.Sheets[desSheetName];
+			Excel.Range desRange = desworksheet.Range[desRangeAdress];
+
+			//Coppy paste value
+
+			sourceRange.Copy();
+			desRange.PasteSpecial(XlPasteType.xlPasteValues, XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+
+			// Sao chép giá trị và định dạng màu sắc
+			sourceRange.Copy();
+			desRange.PasteSpecial(XlPasteType.xlPasteValues, XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+			desRange.PasteSpecial(XlPasteType.xlPasteFormats, XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+
+			// Sao chép định dạng merge và split
+			sourceRange.Copy();
+			desRange.PasteSpecial(XlPasteType.xlPasteColumnWidths, XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+			desRange.PasteSpecial(XlPasteType.xlPasteAll, XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+
+			//Save
+			desWorkbook.Save();
+
+			//Close
+			desWorkbook.Close();
+			sourceWorkbook.Close();
+
+		}
+		public void CloseExcelApp()
+		{
+			excelApp.Quit();
+		}
+
+	}
+}
---
+++ Readexcel/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ReadExcel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string sourceFilePath = "C:\\Users\\Nghia Nano\\Desktop\\sourceTest.xlsx";
+			//string destinationFilePath = "C:\\Users\\Nghia Nano\\Desktop\\desTest.xlsx";
+
+			List<string> dest = new List<string>() { "desTest1.xlsx", "desTest2.xlsx" };
+
+			List<string[]> copyRange = new List<string[]>()
+			{
+				new string[2]{"ád","ads"}
+			};
+
+			foreach(string destItem in dest)
+			{
+				ExcelHelper excelHelper = new ExcelHelper();
+				string destinationFilePath = $"C:\\Users\\Nghia Nano\\Desktop\\{destItem}";
+
+				//CheckTable
+				excelHelper.CopyRange(sourceFilePath, destinationFilePath, "Sheet1", "Sheet1", "B5", "B5");
+
+				//Parameter
+				excelHelper.CopyRange(sourceFilePath, destinationFilePath, "Sheet1", "Sheet1", "B12:B14", "B12");
+
+				excelHelper.CloseExcelApp();
+			}
+
+
+            Console.WriteLine("Done");
+        }
+	}
+}
+
